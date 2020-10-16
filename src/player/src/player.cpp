2af0fe95d1d96dd8be3e2a31c0fcede2674438b7
Zhang_ZW <1009535916@qq.com>
2020-10-16 14:39:31 +0800

    add function to turn and face towards the ball

--- src/player/src/player.cpp
+++ src/player/src/player.cpp
@@ -2,9 +2,14 @@
 
 enum status
 {
+    SEARCHING_BALL,
     REACHING_BALL,
     KICKING_BALL
 } currentStatus;
+
+const float ballThresholdLeft = 0.375, ballThresholdRight = 0.5;
+const float ballThresholdMiddle = (ballThresholdLeft + ballThresholdRight) / 2;
+const float turningPerPixel = 0.02;
 
 int main(int argc, char **argv)
 {
@@ -23,14 +28,15 @@ int main(int argc, char **argv)
     htask.yaw = 0.0;
     htask.pitch = 45.0;
     currentStatus = REACHING_BALL;
-    cv::Vec3f position;
     auto bodyTaskNode = std::make_shared<BodyTaskPublisher>(robotName);
     auto headTaskNode = std::make_shared<HeadTaskPublisher>(robotName);
     auto imageSubscriber = std::make_shared<ImageSubscriber>(robotName);
     auto imuSubscriber = std::make_shared<ImuDataSubscriber>(robotName);
     auto headSubscriber = std::make_shared<HeadAngleSubscriber>(robotName);
     auto resImgPublisher = std::make_shared<ResultImagePublisher>(robotName);
+    auto binImgPublisher = std::make_shared<ResultImagePublisher>("binary");
     rclcpp::WallRate loop_rate(10.0);
+    cv::Vec3f position;
 
     while (rclcpp::ok())
     {
@@ -40,7 +46,7 @@ int main(int argc, char **argv)
         rclcpp::spin_some(imuSubscriber);
         rclcpp::spin_some(headSubscriber);
         rclcpp::spin_some(resImgPublisher);
-        // rclcpp::spin_some(resImgPublisher2);
+        rclcpp::spin_some(binImgPublisher);
         auto imuData = imuSubscriber->GetData();
         auto image = imageSubscriber->GetImage().clone();
         auto headAngle = headSubscriber->GetData();
@@ -49,6 +55,7 @@ int main(int argc, char **argv)
         {
             // Image processing
             auto image = imageSubscriber->GetImage().clone();
+            auto outputImage = image.clone();
             auto imuData = imuSubscriber->GetData();
             auto headAngle = headSubscriber->GetData();
 
@@ -61,14 +68,18 @@ int main(int argc, char **argv)
             if (currentStatus == REACHING_BALL)
             {
                 std::vector<cv::Vec3f> circles;
-                cv::HoughCircles(binImage, circles, cv::HOUGH_GRADIENT, 1, 100, 45, 30, 10, 220);
-                for (size_t i = 0; i < circles.size(); i++)
+                cv::HoughCircles(binImage, circles, cv::HOUGH_GRADIENT, 1, 100, 45, 30, 5, 220);
+                if (circles.size())
                 {
-                    position[0] += circles[i][0];
-                    position[1] += circles[i][1];
-                    position[2] += circles[i][2];
+                    for (size_t i = 0; i < circles.size(); i++)
+                    {
+                        position[0] += circles[i][0];
+                        position[1] += circles[i][1];
+                        position[2] += circles[i][2];
+                    }
+                    position[0] /= circles.size(), position[1] /= circles.size(), position[2] /= circles.size();
+                    cv::circle(outputImage, cv::Point(position[0], position[1]), position[2], cv::Scalar(255, 0, 0), 5);
                 }
-                position[0] /= circles.size(), position[1] /= circles.size(), position[2] /= circles.size();
             }
             else if (currentStatus == KICKING_BALL)
             {
@@ -76,19 +87,21 @@ int main(int argc, char **argv)
             }
 
             cv::cvtColor(binImage, binImage, cv::COLOR_GRAY2BGR);
-            if (currentStatus == REACHING_BALL)
-            {
-                cv::circle(binImage, cv::Point(position[0], position[1]), position[2], cv::Scalar(0, 255, 255), 5);
-            }
-            else if (currentStatus == KICKING_BALL)
-            {
-                //TODO
-            }
-            resImgPublisher->Publish(binImage); // 处理完的图像可以通过该方式发布出去，然后通过rqt中的image_view工具查看
+            binImgPublisher->Publish(binImage);
+            resImgPublisher->Publish(outputImage); // 处理完的图像可以通过该方式发布出去，然后通过rqt中的image_view工具查看
         }
         // Actions
-        if (currentStatus == REACHING_BALL)
+        if (currentStatus == REACHING_BALL && position[2] != 0)
         {
+            if (position[0] > ballThresholdRight * image.cols || position[0] < ballThresholdLeft * image.cols)
+            {
+                btask.set__turn((ballThresholdMiddle * image.cols - position[0]) * turningPerPixel);
+                btask.set__step(0);
+            }
+            else
+            {
+                btask.set__step(1);
+            }
         }
 
         bodyTaskNode->Publish(btask);
